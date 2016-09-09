c502d5e7c1b9dac1fa2fb3f5be00adbdf8d0625e
bici-zhuguangzu <bici_zhuguangzu@outlook.com>
2016-09-09 17:11:32 +0800

    sunday

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,10 @@ Rails.application.routes.draw do
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/signup', to: 'users#new', via: 'get'
+  match '/edit', to: 'users#edit', via: 'get'
   match '/signin',  to: 'sessions#new', via: 'get'
   match '/signout', to: 'sessions#destroy', via: 'delete'
+  match '/indes', to: 'users#index', via: 'get'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
